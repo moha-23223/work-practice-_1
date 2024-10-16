1f8a646cc855313105872ed8f9ebc8d8cdf23675
MOHANAD KHALEEL <150276506+moha-23223@users.noreply.github.com>
2024-10-16 21:15:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const currentYear = 2024
+
+student = "Илья";
+let year = 2006;
+let age = currentYear - year;
+
+console.log("student",student);
+console.log("year of birth", year);
+console.log("age", age);
